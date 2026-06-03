f95a495a3422700f5929778a8db6be7246c2053e
AdarshGrewal <adarshgrewal@gmail.com>
2026-06-03 12:24:42 +0530

    peridot-miuicamera: Use xiaomi camera hwmodule
    Change-Id: I2f722c2c552d94f6c6504d731733ce8be8e0cac8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,7 @@ MIUICAMERA_PATH := device/xiaomi/peridot-miuicamera
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+TARGET_VENDOR_PROP += $(MIUICAMERA_PATH)/vendor.prop
 
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
