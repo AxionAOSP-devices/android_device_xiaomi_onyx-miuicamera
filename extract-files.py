86923e16679b3019a85bb7de446b62cddf7e5add
AdarshGrewal <adarshgrewal@gmail.com>
2026-06-04 16:43:47 +0530

    onyx-miuicamera: Rebrand for onyx
    Change-Id: Iac3636f43978016dacfaa204751b45b3dea3dce0

--- extract-files.py
+++ extract-files.py
@@ -17,7 +17,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/peridot-miuicamera',
+    'device/xiaomi/onyx-miuicamera',
 ]
 
 blob_fixups: blob_fixups_user_type = {
@@ -33,7 +33,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'peridot-miuicamera',
+    'onyx-miuicamera',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
